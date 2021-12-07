74c4f2ea9b7eefa5fba1e5889973ebad374acb6c
[rebecka.gamble] <rebeckagamble@hotmail.com>
2021-12-07 10:45:48 +0100

    kommentarer och scss

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,75 +1,59 @@
 
-//list.map( e=> console.log(e) )
-
-//Skapa en knapp för att kunna välja om det ska adderas eller subtrahera
+    //Välj + eller - beroende på om det ska adderas eller subtraheras. 
 
 var inkomstLista =[];
 var kostnadsLista =[];
 
-
+    //Välj knapp, för att få ned summan till antingen inköps eller kostnadslistan. 
 document.querySelector("button").addEventListener("click", choose);
 
 
 function choose(e){
     e.preventDefault();
-    
-    console.log("button works")
 
+    let costSum =0;
+    let incomeSum =0;
     var beskrivning = document.querySelector("#Beskrivning").value
     var amount = document.querySelector("#summa").value
 
-            //Beroende på om det ska adderas eller subtraheras hamnar summan i Inkomst alternativt Kostnads-listan 
+        //Beroende på om det ska adderas eller subtraheras hamnar summan i Inkomst alternativt Kostnads-listan 
     
 
-    const plusOption = document.querySelector("#choose");
-    console.log(plusOption) 
+        const option = document.querySelector("#choose").value;
 
-        if(plusOption.value=="plus") { 
+        if(option == "plus") { 
             document.querySelector(".inkomstList").innerHTML +=" <li>" +beskrivning +" : " + amount + "</li>" ; inkomstLista.push(amount)  }
 
-  //  const minusOption = document.querySelector(".minus").textContent;
-   // console.log(minusOpti
-        else if(minusOption.value=="minus") { 
-            document.querySelector(".inkomstList").innerHTML +=" <li>" +beskrivning -" : " + amount + "</li>" ; kostnadsLista.push(amount)  }
+        // const minusOption = document.querySelector(".minus").textContent;
+        // console.log(minusOption
+        else if(option == "minus") { 
+            document.querySelector(".kostnadsLista").innerHTML +=" <li>" + beskrivning +" : " + amount + "</li>" ; kostnadsLista.push(amount) 
+            console.log(kostnadsLista)
+        }
+        //Felmedelande visas om man inte väljer + eller -. 
+        else if (option == "-") {
+            alert("Du måste välja + eller -");
+        }
 
+        else {
+            alert("Fyll i formuläret")
+        }
 
-   var summa = 0;
+        //Totalsumman av Inkomster - utgifter. 
+
+    var summa = 0;
 
    // loopa igenom inkomstLista och kostnadLista 
 
-  
-   
-   
+   inkomstLista.map( (e)=> {
+ 
+    incomeSum += parseFloat(e)
+})
 
-   document.querySelector(".vinst").innerHTML= summa;
-   
+//console.log(incomeSum)
+kostnadsLista.map( (e)=> {
+costSum += parseFloat (e)
+})
+   document.querySelector(".totalSum").textContent = incomeSum - costSum;
    
 }
-//var plusOption =
-
-
-
-
-
-/*
-const plus document.querySelector(".beskrivning");
-console.log(Inkomst)
-
-//const plus document.querySelector(".plus").textContent;
-//console.log(plus)
-
-const minus document.querySelector(".minus").textContent;
-console.log(minus)
-
-if(addOption=="+"){
-    
-    const description = documetn.querySelector("Inkomst")
-}
-
-else if(addOption=="-"){
-    
-    const description = documetn.querySelector("Kostnad")
-}
-
-const sortedList = list.filter( e=> e>44 )
-console.log(sortedList)*/
